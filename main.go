a62d6f9dd9444b757eaf06d807d35310064abbe6
FloydWatson <floydbcseit@gmail.com>
2019-10-08 12:18:43 +1300

    added login functions

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -31,10 +33,64 @@ func main() {
 // LOGIN
 func login(w http.ResponseWriter, r *http.Request) {
 
+	var userLogin User
+	req, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(req, &userLogin)
+
 }
 
+// LOGIN FUNC CHECK USERNAME
+func checkUsername(username string) bool {
+	var name string
+
+	db := connectDatabase() // db connection
+	defer db.Close()        // close db connection after use
+
+	getUserName, err := db.Prepare("Select user_name FROM _user WHERE user_name = $1;") // sql query sent to db $1 is the user name
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = getUserName.QueryRow(username).Scan(&name) // sending query
+
+	if err == sql.ErrNoRows {
+		return false // return false if username does not exist
+	}
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return true // return true if user name exists
+}
+
+// LOGIN FUNC CHECK PASSWORD
+func validatePass(password string) bool {
+	var pass string
+
+	db := connectDatabase() // db connection
+	defer db.Close()        // close db connection after use
+
+	stmt, err := db.Prepare("SELECT password FROM _user WHERE password = $1;") // sql query sent to db $1 is the password
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = stmt.QueryRow(password).Scan(&pass) // sending query to db
+
+	if err == sql.ErrNoRows {
+		return false // return false if not found
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return true // return true if correct
+
+}
 
 // MODEL USER
 type User struct {
-	user
+	GivenName  string `json:"givenName"`
+	FamilyName string `json:"familyName"`
+	UserName   string `json:"userName"`
+	Password   string `json:"pasword"`
 }
